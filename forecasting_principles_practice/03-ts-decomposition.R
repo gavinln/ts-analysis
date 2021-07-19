7810e76bd6a78b30a8f5c4dbe483bf68fe73f3cc
Gavin Noronha <gavinln@hotmail.com>
2021-07-18 17:07:22 -0700

    add time series examples

--- forecasting_principles_practice/03-ts-decomposition.R
+++ forecasting_principles_practice/03-ts-decomposition.R
@@ -14,40 +14,40 @@ pacman::p_load(fpp3)
 
 head(global_economy)
 
-global_economy %>% 
-  filter(Country == "Australia") %>% 
-  autoplot(GDP/Population) + 
+global_economy %>%
+  filter(Country == "Australia") %>%
+  autoplot(GDP/Population) +
   labs(title="GDP per capita", y="$US")
 
-aus_economy <- global_economy %>% 
+aus_economy <- global_economy %>%
   filter(Code == "AUS")
 
 glimpse(aus_economy)
 
 glimpse(aus_retail)
 
-print_retail <- aus_retail %>% 
-  filter(Industry == "Newspaper and book retailing") %>% 
-  group_by(Industry) %>% 
-  index_by(Year = year(Month)) %>% 
+print_retail <- aus_retail %>%
+  filter(Industry == "Newspaper and book retailing") %>%
+  group_by(Industry) %>%
+  index_by(Year = year(Month)) %>%
   summarise(Turnover = sum(Turnover))
 
 # inflation adjusted Australian print media
 
-aus_print_turnover <- print_retail %>% 
-  left_join(aus_economy, by = "Year") %>% 
-  mutate(Adjusted_turnover = Turnover / CPI * 100) %>% 
+aus_print_turnover <- print_retail %>%
+  left_join(aus_economy, by = "Year") %>%
+  mutate(Adjusted_turnover = Turnover / CPI * 100) %>%
   pivot_longer(c(Turnover, Adjusted_turnover),
-               values_to = "Turnover") %>% 
+               values_to = "Turnover") %>%
   mutate(name = factor(name, levels=c("Turnover", 'Adjusted_turnover"')))
 
-aus_print_turnover %>% 
-  ggplot(aes(x = Year, y = Turnover)) + 
-  geom_line() + 
-  facet_grid(name ~ ., scales = "free_y") + 
+aus_print_turnover %>%
+  ggplot(aes(x = Year, y = Turnover)) +
+  geom_line() +
+  facet_grid(name ~ ., scales = "free_y") +
   labs(title = "Turnover; Australian print media industry",
        y = "$AU")
-  
+
 # 3.2 Time series components ==============================================
 
 # additive decomposition
@@ -64,23 +64,23 @@ aus_print_turnover %>%
 # U.S. retail sector employment
 head(us_employment)
 
-us_retail_employment <- us_employment %>% 
-  filter(year(Month) >= 1990, Title == "Retail Trade") %>% 
+us_retail_employment <- us_employment %>%
+  filter(year(Month) >= 1990, Title == "Retail Trade") %>%
   select(-Series_ID)
 
-autoplot(us_retail_employment, Employed) + 
+autoplot(us_retail_employment, Employed) +
   labs(y = "Person (thousands)",
        title = "Total employment in US retail")
 
 # STL decomposition
-dcmp <- us_retail_employment %>% 
+dcmp <- us_retail_employment %>%
   model(stl = STL(Employed))
 components(dcmp)
 
-components(dcmp) %>% 
-  as_tsibble() %>% 
-  autoplot(Employed, color = "gray") + 
-  geom_line(aes(y=trend), color = "#D44E00") + 
+components(dcmp) %>%
+  as_tsibble() %>%
+  autoplot(Employed, color = "gray") +
+  geom_line(aes(y=trend), color = "#D44E00") +
   labs(x = "Persons (thousands)",
        title = "Total employment in US retail")
 
@@ -88,9 +88,9 @@ components(dcmp) %>% autoplot()
 
 # seasonally adjusted data
 
-components(dcmp) %>% 
-  as_tsibble() %>% 
-  autoplot(Employed, color="gray") + 
+components(dcmp) %>%
+  as_tsibble() %>%
+  autoplot(Employed, color="gray") +
   geom_line(aes(y=season_adjust), colour = "#0072B2") +
   labs(y = "Persons (thousands)",
        title = "Total employment in US retail")
@@ -98,24 +98,47 @@ components(dcmp) %>%
 
 # 3.3 Moving averages =====================================================
 
-global_economy %>% 
-  filter(Country == "Australia") %>% 
-  autoplot(Exports) + 
+global_economy %>%
+  filter(Country == "Australia") %>%
+  autoplot(Exports) +
   labs(y = "% of GDP",
        title = "Total Australian exports")
-  
-aus_exports <- global_economy %>% 
-  filter(Country == "Australia") %>% 
+
+aus_exports <- global_economy %>%
+  filter(Country == "Australia") %>%
   mutate(S_MA = slider::slide_dbl(
     Exports, mean, .before = 2, .after = 2, complete = TRUE))
 
-aus_exports %>% 
+aus_exports %>%
   autoplot(Exports) +
   geom_line(aes(y = S_MA), color = "#D55E00") +
   labs(y = "% of GDP",
-       title = "Total Australian exports") + 
+       title = "Total Australian exports") +
   guides(color = guide_legend(title = "series"))
-  
+
+# 3.4 Classical decomposition =============================================
+
+# not used
+
+# 3.5 Methods used by official statistics agencies ========================
+
+# 3.6 STL decomposition ===================================================
+
+# Seasonal and Trend decomposition using Loess
+
+us_retail_employment %>%
+  model(
+    STL(Employed ~ trend(window = 7) +
+          season(window = "period"),
+        robust=TRUE)) %>%
+  components() %>%
+  autoplot()
+
+
+head(us_retail_employment)
+
+glimpse(us_retail_employment)
+
 # Clean up
 p_unload(all)  # clears all add-ons
 
