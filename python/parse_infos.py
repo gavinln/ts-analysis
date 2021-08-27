b4dec3536def74b34e8e2851fe45a1561182620e
Gavin Noronha <gavinln@hotmail.com>
2021-08-26 22:26:40 -0700

    add pyparsing examples

---
+++ python/parse_infos.py
@@ -0,0 +1,29 @@
+# https://scipy-cookbook.readthedocs.io/items/Reading_Custom_Text_Files_with_Pyparsing.html
+import pathlib
+from pyparsing import Word, SkipTo, Optional, alphanums, lineEnd, empty
+from pyparsing import Suppress
+
+
+SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
+
+
+# parameter definition
+keyName = Word(alphanums + '_')
+
+LParen, RParen = map(Suppress, '()')
+unitDef = LParen + Word(alphanums + '^*/-._') + RParen
+paramValueDef = SkipTo('#' | lineEnd)
+
+paramDef = keyName + Optional(unitDef) + "=" + empty + paramValueDef
+
+
+def test_paramDef():
+    print(paramDef.parseString('Debug = False'))
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
